bace45d4145ef09ef840825561bd795dc7f047d5
JHL-HK <valor@jianyuelab.org>
2025-10-25 19:26:46 +0900

    Delete Unuse apps and modify starship

--- home.nix
+++ home.nix
@@ -66,25 +66,7 @@
     enable = true;
     # 自定义配置
     settings = {
-      add_newline = false;
-      aws.disabled = true;
-      gcloud.disabled = true;
-      line_break.disabled = true;
-    };
-  };
-
-  # alacritty - 一个跨平台终端，带 GPU 加速功能
-  programs.alacritty = {
-    enable = true;
-    # 自定义配置
-    settings = {
-      env.TERM = "xterm-256color";
-      font = {
-        size = 12;
-        draw_bold_text_with_bright_colors = true;
-      };
-      scrolling.multiplier = 5;
-      selection.save_to_clipboard = true;
+      add_newline = true;
     };
   };
 
